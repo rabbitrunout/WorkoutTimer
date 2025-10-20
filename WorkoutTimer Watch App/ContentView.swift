11a9e11bcbbba4c362158ae602af1a01954c09ab
Irina Saf <irina.safronova0801@gmail.com>
2025-10-20 08:18:03 -0400

    Initial Commit

---
+++ WorkoutTimer Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutTimer Watch App
+//
+//  Created by Irina Saf on 2025-10-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutTimer Watch App/WorkoutTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTimerApp.swift
+//  WorkoutTimer Watch App
+//
+//  Created by Irina Saf on 2025-10-20.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTimer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
